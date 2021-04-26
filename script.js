d2d23b988c49340261eeae49b8e59782386f1050
Austin Whitbeck <austinwhitbeckart@gmail.com>
2021-04-26 18:38:55 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,34 @@
+// Functions needed
+
+// getAreaOfCircle(radius)
+
+function getAreaOfCircle(radius){
+   return area = 3.14 * (radius * radius);
+}
+
+console.log(getAreaOfCircle(4));
+
+// getCircumferenceOfCircle(radius)
+
+function getCircumferenceOfCircle(radius){
+    return circumference = 2 * 3.14 * radius;
+}
+ 
+ console.log(getCircumferenceOfCircle(4));
+
+// getAreaOfSquare(side)
+
+function getAreaOfSquare(side){
+    return area = side * side;
+}
+ 
+ console.log(getAreaOfSquare(4));
+
+// getAreaOfTriangle(base,height)
+
+function getAreaOfTriangle(base,height){
+    return area = (height * base) / 2;
+}
+ 
+ console.log(getAreaOfTriangle(4,7));
+
